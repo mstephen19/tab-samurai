236642fdd47d926ef5c7b30af75e738963ce6106
mstephens19 <matthias.stephens@tapcart.co>
2025-01-15 01:11:28 -0800

    fix assets

--- site/next.config.ts
+++ site/next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
     output: 'export',
     basePath: '/tab-samurai',
+    assetPrefix: '/tab-samurai/',
 };
 
 export default nextConfig;
