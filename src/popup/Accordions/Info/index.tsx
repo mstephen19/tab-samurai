3c0ff21973c30882a8833418adf9d1c4b1f24e79
mstephens19 <matthiasvstephens@gmail.com>
2025-02-02 13:19:10 -0800

    feat(Info): icons & copy update

--- src/popup/Accordions/Info/index.tsx
+++ src/popup/Accordions/Info/index.tsx
@@ -1,6 +1,9 @@
 import { Box, Divider, List, ListItem, ListItemText } from '@mui/material';
-import { FEATURE_REQUEST_URL, HELP_AND_INFO_FAQ, REPORT_BUG_URL } from '../../../consts';
+import { FEATURE_REQUEST_URL, HELP_AND_INFO_FAQ, REPORT_BUG_URL, REVIEW_EXTENSION_URL } from '../../../consts';
 import { Link } from '../../components/Link';
+import TipsAndUpdatesIcon from '@mui/icons-material/TipsAndUpdates';
+import BugReportIcon from '@mui/icons-material/BugReport';
+import StarIcon from '@mui/icons-material/Star';
 
 export const Info = () => {
     return (
@@ -15,10 +18,24 @@ export const Info = () => {
 
             <Divider flexItem />
 
-            <Box display='flex' gap='10px'>
-                <Link url={FEATURE_REQUEST_URL}>Request a Feature</Link>
+            <Box display='flex' justifyContent='space-between'>
+                <Box component='span' display='flex' alignItems='center' gap='5px'>
+                    <TipsAndUpdatesIcon sx={{ fontSize: '1rem' }} />
 
-                <Link url={REPORT_BUG_URL}>Report a Bug</Link>
+                    <Link url={FEATURE_REQUEST_URL}>Request a Feature</Link>
+                </Box>
+
+                <Box component='span' display='flex' alignItems='center' gap='5px'>
+                    <BugReportIcon sx={{ fontSize: '1rem' }} />
+
+                    <Link url={REPORT_BUG_URL}>Report a Bug</Link>
+                </Box>
+
+                <Box component='span' display='flex' alignItems='center' gap='5px'>
+                    <StarIcon sx={{ fontSize: '1rem' }} />
+
+                    <Link url={REVIEW_EXTENSION_URL}>Leave a Review</Link>
+                </Box>
             </Box>
         </Box>
     );
