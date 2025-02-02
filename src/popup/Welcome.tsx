d04979b1e3f783f9578bcefdba5fcefd26457ba6
mstephens19 <matthiasvstephens@gmail.com>
2025-02-02 14:37:15 -0800

    perf: confetti only imports if needed

--- src/popup/Welcome.tsx
+++ src/popup/Welcome.tsx
@@ -1,8 +1,9 @@
-import { memo, useContext, useEffect } from 'react';
+import { Suspense, lazy, memo, useContext, useEffect } from 'react';
 import sword from '../assets/sword.mp3';
 import { AppDataContext } from './context/AppDataProvider';
 import { store } from '../storage';
-import Confetti from 'react-confetti';
+
+const Confetti = lazy(() => import('react-confetti'));
 
 export const Welcome = memo(() => {
     const appData = useContext(AppDataContext);
@@ -21,25 +22,29 @@ export const Welcome = memo(() => {
         };
     }, [appData.userWelcomed]);
 
+    if (appData.userWelcomed) return null;
+
     return (
-        <Confetti
-            recycle={false}
-            run={!appData.userWelcomed}
-            tweenDuration={2_500}
-            opacity={0.8}
-            width={600}
-            height={600}
-            gravity={0.45}
-            initialVelocityX={4}
-            initialVelocityY={15}
-            numberOfPieces={200}
-            colors={['#FFC238', '#C64635', '#FFD55D', '#70291E']}
-            onConfettiComplete={() => {
-                store.appData.write({
-                    ...appData,
-                    userWelcomed: true,
-                });
-            }}
-        />
+        <Suspense fallback={null}>
+            <Confetti
+                recycle={false}
+                run={!appData.userWelcomed}
+                tweenDuration={2_500}
+                opacity={0.8}
+                width={600}
+                height={600}
+                gravity={0.45}
+                initialVelocityX={4}
+                initialVelocityY={15}
+                numberOfPieces={200}
+                colors={['#FFC238', '#C64635', '#FFD55D', '#70291E']}
+                onConfettiComplete={() => {
+                    store.appData.write({
+                        ...appData,
+                        userWelcomed: true,
+                    });
+                }}
+            />
+        </Suspense>
     );
 });
